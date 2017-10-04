14f3584e8b2d3374494da99bdef842e9e8a1ffca
Craig Hammond <craigham94@gmail.com>
2017-10-04 18:26:07 -0400

    condensed styling in App, reworded message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const APP_STYLE = {
-  textAlign: "center"
-}
-
 class App extends Component {
   render() {
     return (
-      <div style={APP_STYLE}>
+      <div style={{
+        textAlign: "center"
+      }}>
         Hello, welcome to craighammond.me!
         <br/><br/>
-        There's not a lot going on here right now.
+        There's not a lot going on here yet.
         For now, you can check out
         my <a href="https://www.linkedin.com/in/hammondcraigr/">LinkedIn</a>.
         Or not. I'm a website not a cop.
